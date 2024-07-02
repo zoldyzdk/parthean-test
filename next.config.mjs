b4ba05298708029b81607b91f2736d48e5d4fcee
Hewerton <hewerton.soares@yahoo.com>
2024-07-01 23:43:23 -0300

    chore: add img config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.prod.website-files.com",
+        port: "",
+        pathname: "/652dbebd0c8e37e771b32d9c/**"
+      }
+    ]
+  }
+};
 
 export default nextConfig;
