5307b979cf775d2f6ae802b2bbf0f1700ef25ec6
Hewerton <hewerton.soares@yahoo.com>
2024-07-02 12:14:57 -0300

    feat: create beta tag component

---
+++ src/components/beta-tag.tsx
@@ -0,0 +1,7 @@
+export function BetaTag() {
+ return (
+  <div className="h-7 uppercase py-1 px-3 mx-2 text-[10px] text-[rgba(0,0,0,.5)] flex items-center rounded-[20px] bg-gradient-to-r from-[rgba(0,0,0,.04)] to-[rgba(0,0,0,.04)]">
+   beta
+  </div>
+ );
+}
